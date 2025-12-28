8a669d6219e59c6efa5cdf36cc8ea6fb3e82aba8
Lily <b251940@gmail.com>
2025-12-28 16:00:19 +0800

    add def generation

--- RAG_ex1.py
+++ RAG_ex1.py
@@ -1,2 +1,24 @@
+from openai import OpenAI
+import os
+api_key = os.environ['API_KEY']
+
+client = OpenAI() #base_url api_key
+
+
 def retrieval(query):
     return 
+
+def generation(prompt):
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return completion.choices[0].message
+

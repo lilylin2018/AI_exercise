56ea1e0dda74f6ef34cf1e54a0accbcb2bd0c8cd
Lily <b251940@gmail.com>
2025-12-31 00:37:30 +0800

    2025/12/31 exercise

--- RAG_ex1.py
+++ RAG_ex1.py
@@ -6,7 +6,23 @@ client = OpenAI() #base_url api_key
 
 
 def retrieval(query):
-    return 
+    context = ""
+    #向量搜索
+    #query相關文件添加到context
+
+    return context 
+
+def augmented(query, context=""):
+    if not context:
+        return f"請回答以下問題: {query}"
+    else:
+        prompt = f"""請根據上下文訊息來回答問題，如果上下文訊息不足以回答問題，請直接說:"根據提供的上下文訊息，無法回答"
+        上下文:
+        {context}
+
+        問題:{query}"""
+
+        return prompt
 
 def generation(prompt):
     completion = client.chat.completions.create(
@@ -22,3 +38,12 @@ def generation(prompt):
 
     return completion.choices[0].message
 
+
+if __name__ == '__main__':
+    query = "who are you?"
+
+    context = retrieval(query)
+    prompt = augmented(query, context)
+    resp = generation(prompt)
+    
+    print(resp)
